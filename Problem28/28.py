c60c72884841c526e20a301efc1e04c13a62fa50
rahmanto@lenovo <rahmanto@outlook.com>
2024-10-28 00:08:26 +0900

    still working prob28

---
+++ Problem28/28.py
@@ -0,0 +1,13 @@
+# Number Spiral Diagonals
+# Starting with number 1 and moving to the right in the clockwise direction a 5 by 5 spiral is formed.
+# It can be verified that the sum of the numbers on the diagonals is 101.
+# What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+
+spiral = [[]]
+
+side = 5
+mid_index = side // 2
+
+for y in range(1, side + 1):
+    for x in range(
+
