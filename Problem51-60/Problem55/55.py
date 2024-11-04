2c58e41dd83a4a9ca148b98f73b5d7c9f5a802a2
rahmanto <rahmanto@iedapad>
2024-11-04 15:25:40 +0900

    completed problem 55

--- Problem51-60/Problem55/55.py
+++ Problem51-60/Problem55/55.py
@@ -3,18 +3,27 @@
 def ispalindrom(num):
     return str(num) == str(num)[::-1]
 
-def islychrel(num):
-    reversed_num = str(num)[::-1]
-    if ispalindrom(num + int(reversed_num)):
-        return True
-    else:
-        return False
-
 def count_lychrel_num(limit):
     count = 0
     for num in range(1,limit+1):
-        num_reversed = int(str(num))
-        if ispalindromic(num + num_reversed):
+        iterations = 0
+        found = False
+        curr_num = num
+        while iterations < 50 and not found:
+            curr_num_reversed = int(str(curr_num)[::-1])
+            check_num = curr_num + curr_num_reversed
+            if ispalindrom(check_num):
+                found = True
+            else:
+                curr_num = check_num
+            
+            iterations += 1
+        
+        if not found:
             count+=1
+    
+    return count
 
-print(ispalindromic(1002))
+limit = 10000
+print(count_lychrel_num(limit))
+# print(ispalindrom(4994))
